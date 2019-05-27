de4c35d553cbe648c7f7959c3cc7b50eb2c9124b
xPudimx <hugodias.hmmd@gmail.com>
2019-05-27 17:07:07 +0100

    Ficha8

--- Ficha8/Q3.c
+++ Ficha8/Q3.c
@@ -1,29 +1,37 @@
+#include <sys/wait.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+
 #define READ 0
 #define WRITE 1
 #define LINESIZE  256
+
 int main(int argc, char* argv[]) {
 	int   n, r, fd[2];
 	pid_t pid;
 	char  line[LINESIZE];
 	if (pipe(fd) < 0) {
+		perror("pipe error");
 		exit(EXIT_FAILURE);
 	}
 	if ((pid = fork()) < 0) {
+		perror("fork error");
 		exit(EXIT_FAILURE);
 	}
 	else if (pid > 0) {
 		/* parent */
 		close(fd[WRITE]);
 		while(r = read(fd[READ], line, LINESIZE) != 0) {
-
 		write(STDOUT_FILENO, line, r);
 	}
 		close(fd[READ]);
 		/* wait for child and exit */
+		if(waitpid(pid, NULL, 0) < 0){
+			fprintf(stderr, "cannot wait for child: %s\n", strerror(errno));
+		}
 		exit(EXIT_SUCCESS);
 	}
 	else {
